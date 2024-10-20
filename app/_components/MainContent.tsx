155358ef938ab4b15642079dd2be50fd41d83f36
drealdumore <sireroyce1@gmail.com>
2024-10-21 00:13:19 +0100

    refactor: UI

--- app/_components/MainContent.tsx
+++ app/_components/MainContent.tsx
@@ -23,8 +23,8 @@ const MainContent = ({
       {error && <ErrorMessage message={error} />}
       {userData && <UserProfile userData={userData} />}
       {repositories.length > 0 && (
-        <div className="flex flex-col gap-54md:gap-8 items-center justify-center">
-          <h3 className="text-2xl font-semibold self-start sm:self-center sm:pb-4">Repositories:</h3>
+        <div className="flex flex-col gap-4 md:gap-8 items-center justify-center">
+          <h3 className="text-xl font-semibold self-start sm:self-center sm:pb-4 sm:text-2xl">Repositories:</h3>
 
           <div className="grid max-w-6xl grid-cols-1 overflow-hidden sm:grid-cols-2 lg:grid-cols-3 [&>*]:relative [&>*]:px-1 [&>*]:py-4 [&>*]:before:absolute [&>*]:before:bg-[#e5e7eb]/70 [&>*]:before:dark:bg-[#333333]/70 [&>*]:before:[block-size:100vh] [&>*]:before:[inline-size:1px] [&>*]:before:[inset-block-start:0] [&>*]:before:[inset-inline-start:-1px] [&>*]:after:absolute [&>*]:after:bg-[#e5e7eb]/70 [&>*]:after:dark:bg-[#333333]/70 [&>*]:after:[block-size:1px] [&>*]:after:[inline-size:100vw] [&>*]:after:[inset-block-start:-1px] [&>*]:after:[inset-inline-start:0] sm:[&>*]:px-8 xl:[&>*]:px-12">
             <RepositoryTable repositories={repositories} />
